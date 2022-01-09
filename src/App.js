0f2a9e0c216dde19c0b4dd7ed55ec6b33ecbae7a
juhyoungryu <thinkoutside2004@gmail.com>
2022-01-09 12:15:56 +0900

    spring 라이브러리를 활용한 화면 전환 연습

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import "./App.css";
 import React, { useState } from "react";
+import { useTransition, animated } from "react-spring";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const transition = useTransition(isVisible, {
+    from: { x: -100, y: 500, opacity: 0, scale: 0, delay: 0 },
+    enter: { x: 0, y: 0, opacity: 1, scale: 1, delay: 400 },
+    leave: { x: 100, y: 500, opacity: 0, scale: 0, delay: -300 },
+  });
+
+  const change = () => {
+    setIsVisible((isVisible) => !isVisible);
+  };
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          change();
+          setTimeout(change, 1500);
+        }}
+      >
+        {isVisible ? "un-mount" : "mount"}
+      </button>
+      <div className="container">
+        {transition((style, item) =>
+          item ? <animated.div className="item" style={style} /> : ""
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default App;
