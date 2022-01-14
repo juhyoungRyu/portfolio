de4f7bffdf3e4a900aa1c6827e82896e4a53c050
juhyoungryu <thinkoutside2004@gmail.com>
2022-01-15 00:02:25 +0900

    About 페이지 구성 시작

--- src/compo/About.js
+++ src/compo/About.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./style.css";
+import "./about.css";
 
 const About = () => {
-  return <div className="About">About</div>;
+  return (
+    <div className="About">
+      <h1>About</h1>
+    </div>
+  );
 };
 
 export default About;
