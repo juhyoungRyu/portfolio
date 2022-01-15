8ddbddd1c9b4422dcea1c0979f9aa287bc8b4890
juhyoungryu <thinkoutside2004@gmail.com>
2022-01-15 13:05:58 +0900

    조정

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # My Portfolio
 
-안녕하세요,, 저는 거북이같은 사람입니다...
+Hi! I'm a Maker!
--- src/compo/About.js
+++ src/compo/About.js
@@ -5,7 +5,14 @@ import "./about.css";
 const About = () => {
   return (
     <div className="About">
-      <h1>About</h1>
+      <h1>안녕하세요!</h1>
+      <h1>저는</h1>
+      <h1>19살</h1>
+      <h1>기획자이자</h1>
+      <h1>개발자인</h1>
+      <h1>메이커,</h1>
+      <h1>유주형</h1>
+      <h1>입니다</h1>
     </div>
   );
 };
