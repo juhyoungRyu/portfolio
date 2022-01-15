99f6182c2bae732f97295086c58366ad5451cb56
juhyoungryu <thinkoutside2004@gmail.com>
2022-01-15 14:02:25 +0900

    깃헙 잔디 연동

--- src/compo/Blog.js
+++ src/compo/Blog.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./style.css";
-
+import GitHubCalendar from "react-github-calendar";
 const Blog = () => {
-  return <div className="Blog">Blog</div>;
+  return (
+    <div className="Blog">
+      <GitHubCalendar
+        username="juhyoungryu"
+        hideMonthLabels={true}
+        hideColorLegend={true}
+        year={new Date().getFullYear()}
+        color={"#98fb98"}
+      />
+    </div>
+  );
 };
 
 export default Blog;
